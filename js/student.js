6701fb6bb6854cb88dee7a1a923ff34336a56647
Yassine El-Brahmi <yassineelbrahmi11@gmail.com>
2024-06-29 16:29:48 +0200

    date instead of age in student class changed

--- js/student.js
+++ js/student.js
@@ -1,11 +1,14 @@
 import { ENDPOINT } from "./constants.js"
 
 export default class Student {  
-    constructor(name, age, note){
+    constructor( name, age, note){
         this.name=  name;
         this.age = age;
         this.note = note;
     }
+    getAge = () => ((new Date()).getFullYear()  - new Date(this.age).getFullYear())
+
+
 
     static allStudents = async function() {
         const response = await fetch(ENDPOINT)
@@ -13,13 +16,17 @@ export default class Student {
         return students;
     }
 
+
     addStudent = async function() {
       const response = await fetch(ENDPOINT, {
 
         method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
         body: JSON.stringify({
           name: this.name,
-          age : this.age,
+          date : this.age,
           note: this.note
 
         })
@@ -27,5 +34,17 @@ export default class Student {
       console.log(response);
       return response;
   }
+
+  static deleteStudent = async function(id) {
+    const response = await fetch(`${ENDPOINT}/${id}`, {
+
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+
+    });
+    return response;
+}
     
 }
