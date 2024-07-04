62f11c4f869b2932d47dba3ec86247571a22ed44
yassine <yassine.el-brahmi@eoa.de>
2024-07-04 16:03:31 +0200

    new formatting

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 import Student from "./student.js";
 
+const DisplayAllStudents = async function () {
+  return Student.allStudents().then(function (response) {
+    // Daten sortieren
+    const sortedData = response.sort((a, b) => b.note - a.note);
 
-const DisplayAllStudents = async function() {
-  return Student.allStudents().then(function(response) {
-      // Daten sortieren
-      const sortedData = response.sort((a, b) => b.note - a.note);
-
-      return sortedData.map((data) => {
-          const { id, name, date, note } = data;
-          const student = new Student(name, date, note);
-          return `
+    return sortedData.map((data) => {
+      const { id, name, date, note } = data;
+      const student = new Student(name, date, note);
+      return `
               <tr>
                   <td>${id}</td>
                   <td>${student.name}</td>
@@ -18,59 +17,56 @@ const DisplayAllStudents = async function() {
                   <td><button class="btn btn-danger btn-sm delete" data-id="${id}">Delete</button></td>
               </tr>
           `;
-      });
+    });
   });
-}
+};
 
-const addStudent = function(event) {
-    event.preventDefault();
-    const [name, date, note ] = document.querySelectorAll("#name, #date, #note")
-    const student = new Student(name.value, date.value, note.value);
-    student.addStudent()
-}
+const addStudent = function (event) {
+  event.preventDefault();
+  const [name, date, note] = document.querySelectorAll("#name, #date, #note");
+  const student = new Student(name.value, date.value, note.value);
+  student.addStudent();
+};
 
-const deleteStudent = function(event) {
-  const id = event.target.getAttribute('data-id');
+const deleteStudent = function (event) {
+  const id = event.target.getAttribute("data-id");
   console.log(id);
   if (id) {
-      Student.deleteStudent(id).then(() => {
-          renderStudents();
-      }).catch(error => {
-          console.error('Error deleting student:', error);
+    Student.deleteStudent(id)
+      .then(() => {
+        renderStudents();
+      })
+      .catch((error) => {
+        console.error("Error deleting student:", error);
       });
   }
-}
-
-
+};
 
 //display the data in HTML
-const renderStudents = function() {
+const renderStudents = function () {
   const body = document.querySelector(".stundets-list");
-  DisplayAllStudents().then(function(data) {
-      if (data) {
-          body.innerHTML = data.join(' ');
-          body.querySelectorAll('.delete').forEach(button => {
-              button.addEventListener('click', deleteStudent);
-          });
-      }
+  DisplayAllStudents().then(function (data) {
+    if (data) {
+      body.innerHTML = data.join(" ");
+      body.querySelectorAll(".delete").forEach((button) => {
+        button.addEventListener("click", deleteStudent);
+      });
+    }
   });
-}
+};
 
+// refresh button
+const init = function () {
+  const refreshButton = document.querySelector("#refresh");
+  const addButton = document.querySelector("#add");
 
-// refresh button 
-const init = function(){
-    const refreshButton = document.querySelector("#refresh");
-    const addButton = document.querySelector("#add");
-
-
-    refreshButton.addEventListener("click", () => {
-        renderStudents();
-    })
-    addButton.addEventListener("click", (event) => {
-        addStudent(event);
-    })
-
-}
+  refreshButton.addEventListener("click", () => {
+    renderStudents();
+  });
+  addButton.addEventListener("click", (event) => {
+    addStudent(event);
+  });
+};
 
 init();
 renderStudents();
--- js/student.js
+++ js/student.js
@@ -1,50 +1,43 @@
-import { ENDPOINT } from "./constants.js"
+import { ENDPOINT } from "./constants.js";
 
-export default class Student {  
-    constructor( name, age, note){
-        this.name=  name;
-        this.age = age;
-        this.note = note;
-    }
-    getAge = () => ((new Date()).getFullYear()  - new Date(this.age).getFullYear())
-
-
-
-    static allStudents = async function() {
-        const response = await fetch(ENDPOINT)
-        const students = await response.json()
-        return students;
-    }
-
-
-    addStudent = async function() {
-      const response = await fetch(ENDPOINT, {
-
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          name: this.name,
-          date : this.age,
-          note: this.note
-
-        })
-      });
-      console.log(response);
-      return response;
+export default class Student {
+  constructor(name, age, note) {
+    this.name = name;
+    this.age = age;
+    this.note = note;
   }
+  getAge = () => new Date().getFullYear() - new Date(this.age).getFullYear();
 
-  static deleteStudent = async function(id) {
+  static allStudents = async function () {
+    const response = await fetch(ENDPOINT);
+    const students = await response.json();
+
+    return students;
+  };
+
+  addStudent = async function () {
+    const response = await fetch(ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: this.name,
+        date: this.age,
+        note: this.note,
+      }),
+    });
+    console.log(response);
+    return response;
+  };
+
+  static deleteStudent = async function (id) {
     const response = await fetch(`${ENDPOINT}/${id}`, {
-
       method: "DELETE",
       headers: {
-        "Content-Type": "application/json"
-      }
-
+        "Content-Type": "application/json",
+      },
     });
     return response;
-}
-    
+  };
 }
